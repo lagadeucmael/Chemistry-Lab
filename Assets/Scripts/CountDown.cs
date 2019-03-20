02035bb326928106a4874433f00b49a2edc6ebde
Douaa <douaa_khalil@hotmail.com>
2019-03-20 16:32:06 +0100

    Chaimaa: ajout countDown et lumiere d alarme

---
+++ Assets/Scripts/CountDown.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+public class CountDown : MonoBehaviour
+{
+
+    public static float timeLeft;
+    public static float time;
+    Text text;
+
+    void Awake()
+    {
+        text = GetComponent<Text>();
+        text.text = "\n" + Mathf.Round(timeLeft);
+        time = 100;
+        timeLeft = time;
+    }
+
+    void Update()
+    {
+
+        if (timeLeft < 0)
+        {
+            text.text = "\n Time Over !!!";
+
+            //Application.LoadLevel("gameOver");
+        }
+        else
+        {
+            timeLeft -= Time.deltaTime;
+            text.text = "\n" + Mathf.Round(timeLeft);
+        }
+    }
+}
---
+++ Assets/Scripts/rotate.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotate : MonoBehaviour
+{
+
+    // Use this for initialization
+    public float smooth = 1f;
+    private Quaternion targetRotation;
+    float angle = 0.1f;
+    float time;
+    float timeLeft;
+    void Start()
+    {
+        targetRotation = transform.rotation;
+        time = CountDown.time;
+    }
+
+    void Update()
+    {
+        timeLeft = CountDown.timeLeft;
+
+        angle = (30f / timeLeft);
+        print("In update true");
+        //targetRotation *= Quaternion.AngleAxis(Mathf.RoundToInt(angle), Vector3.up);
+        targetRotation *= Quaternion.AngleAxis(angle, Vector3.up);
+        transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, 10 * smooth); //* Time.deltaTime);
+    }
+}
