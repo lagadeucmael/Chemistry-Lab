3cd3abcc82682c7289d4c5aed4cde16a894b4562
RoRoChaimaa <aajafirchaimaa@gmail.com>
2019-03-28 17:36:48 +0100

    Adding flasks with particles

---
+++ Assets/Scripts/Instantiation.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instantiation : MonoBehaviour {
+    public GameObject go;
+    public ParticleSystem particles;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Vector3.Dot(go.transform.up, Vector3.up) < 0)
+        {
+            Debug.Log("Particule Emit !!");
+            particles.Emit(1);
+        } else
+        {
+            Debug.Log("Particule Stop !!");
+            //particles.Pause();
+            //particles.Clear();
+            particles.Stop();
+        }
+
+
+	}
+}
